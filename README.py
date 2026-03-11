ea8c536a32f954d48a024061a01e4ffdc3f315cf
JOHN MARK E. LIAGING <johmarkliaging@gmail.com>
2026-03-11 16:01:45 +0800

    Add README.py for basic arithmetic operations

---
+++ README.py
@@ -0,0 +1,35 @@
+# 1. Int Variables
+num1_int = 26
+num2_int = 4
+
+print("-int Operations-")
+print(f"Addition: {num1_int} + {num2_int} = {num1_int + num2_int}")
+print(f"Subtraction: {num1_int} - {num2_int} = {num1_int - num2_int}")
+print(f"Division: {num1_int} / {num2_int} = {num1_int / num2_int}")
+print(f"Multiplication: {num1_int} * {num2_int} = {num1_int * num2_int}")
+print(f"Modulus: {num1_int} % {num2_int} = {num1_int % num2_int}")
+print(f"Exponent: {num1_int} * {num2_int} = {num1_int * num2_int}")
+
+# 2. Float Variables
+num1_float = 30.5
+num2_float = 4.5
+
+print("\n-float Operations-")
+print(f"Addition: {num1_float} + {num2_float} = {num1_float + num2_float}")
+print(f"Subtraction: {num1_float} - {num2_float} = {num1_float - num2_float}")
+print(f"Division: {num1_float} / {num2_float} = {num1_float / num2_float}")
+print(f"Multiplication: {num1_float} * {num2_float} = {num1_float * num2_float}")
+print(f"Modulus: {num1_float} % {num2_float} = {num1_float % num2_float}")
+print(f"Exponent: {num1_float} * {num2_float} = {num1_float * num2_float}")
+
+# 3. Complex Variables
+num1_complex = 5 + 3j
+num2_complex = 2 + 4j
+
+print("\n-complex Operations-")
+print(f"Addition: {num1_complex} + {num2_complex} = {num1_complex + num2_complex}")
+print(f"Subtraction: {num1_complex} - {num2_complex} = {num1_complex - num2_complex}")
+print(f"Division: {num1_complex} / {num2_complex} = {num1_complex / num2_complex}")
+print(f"Multiplication: {num1_complex} * {num2_complex} = {num1_complex * num2_complex}")
+print(f"Modulus: {num1_complex} % {num2_complex} = Not Supported for Complex Numbers")
+print(f"Exponent: {num1_complex} * {num2_complex} = {num1_complex * num2_complex}")
